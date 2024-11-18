4d1a7af70b7993db289683c0c1f168d41b29abc4
Erik Liu <erik.liu@spk.no>
2024-11-18 14:49:11 +0100

    feat: oppgave 5 backendkode leser high_score fra db

--- oppgave-5_compose/resources/backend/server.py
+++ oppgave-5_compose/resources/backend/server.py
@@ -11,7 +11,7 @@ high_score_db = "db/high_score"
 def read_high_score():
     if os.path.isfile(high_score_db):
         with open(high_score_db, 'r') as file:
-            file_content = file.read()
+            file_content = file.read().strip()
             if file_content.isnumeric():
                 return int(file_content)
     return 0
@@ -24,20 +24,18 @@ def update_high_score(new_score):
         file.write(str(new_score))
 
 
-high_score = read_high_score()
 score = 0
 
 
 @app.get("/play")
 async def play():
     global score
-    global high_score
     result = random.randint(-1, 1)
     if result > 0:
         score += 1
-        if score > high_score:
-            high_score = score
-            update_high_score(high_score)
+        if score > read_high_score():
+            update_high_score(score)
+
     elif result < 0:
         score = 0
 
@@ -52,5 +50,4 @@ async def get_score():
 
 @app.get("/high_score")
 async def get_high_score():
-    global high_score
-    return {"high_score": high_score}
+    return {"high_score": read_high_score()}
